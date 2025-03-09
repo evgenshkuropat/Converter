160eccc7ec54bcb964469e5b991d82ec1b702fef
evgenshkuropat <evgenshkuropat@gmail.com>
2025-03-09 23:50:31 +0100

    Added function to convert kilometers to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,10 +8,18 @@ public class Main {
         return miles * MILE_TO_KM;
     }
 
+    // Метод для конвертації кілометрів у милі
+    private static double convertKmToMiles(double km) {
+        return km / MILE_TO_KM;
+    }
+
     public static void main(String[] args) {
         System.out.println("Converter App: Miles ↔ Kilometers");
 
         double miles = 5;
+        double km = 8;
+
         System.out.println(miles + " miles = " + convertMilesToKm(miles) + " km");
+        System.out.println(km + " km = " + convertKmToMiles(km) + " miles");
     }
 }
